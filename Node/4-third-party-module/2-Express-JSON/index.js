ade718b0956ff85472e5f11e056c6c310e4b21b4
skumarscse-skillsafari <sathish.kumar@skillsafari.in>
2024-03-12 11:48:06 +0530

    express-json-2

--- Node/4-third-party-module/2-Express-JSON/index.js
+++ Node/4-third-party-module/2-Express-JSON/index.js
@@ -1,8 +1,31 @@
 import express from "express";
 import products from "./data.js";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 const PORT = 5000;
+app.use(express.json());
+// CRUD
+// Create => POST
+// Read => GET
+// Update => PUT/PATCH
+// Delete => DELETE
+
+// Users
+// CreateUser
+// POST => http://localhost:5000/api/v1/users => data
+
+// getAllUsers
+// GET => http://localhost:5000/api/v1/users
+
+// getUserById
+// GET => http://localhost:5000/api/v1/users/:id => id
+
+// updateUserById
+// PUT => http://localhost:5000/api/v1/users/:id => data + id
+
+// deleteById
+// DELETE => http://localhost:5000/api/v1/users/:id => id
 
 app.get("/", (req, res) => {
   res.status(200).json({ success: true, message: "Welcome to Express JSON" });
@@ -54,6 +77,53 @@ app.get("/api/v1/product", async (req, res) => {
       return res.status(200).json({ success: true, message: "No match found" });
     const limitedProducts = filteredProducts.slice(0, limit);
     res.status(200).json({ success: true, data: limitedProducts });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error });
+  }
+});
+
+app.post("/api/v1/products", async (req, res) => {
+  try {
+    // console.log(req.body);
+    const newProducts = { id: uuidv4(), ...req.body }; // {title: , description: }
+    res.status(201).json({
+      success: true,
+      message: "Products added successfully",
+      data: newProducts,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error });
+  }
+});
+
+app.put("/api/v1/products/:id", async (req, res) => {
+  try {
+    // id and data
+    // id => req.params
+    // data => req.body
+    const { id } = req.params;
+    const updatedProducts = products.map((product) =>
+      product.id === +id ? { id: id, ...req.body } : product
+    );
+    res.status(200).json({
+      success: true,
+      message: "Product updated successfully",
+      data: updatedProducts,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error });
+  }
+});
+
+app.delete("/api/v1/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const newProducts = products.filter((product) => product.id !== +id);
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+      data: newProducts,
+    });
   } catch (error) {
     res.status(400).json({ success: false, message: error });
   }
